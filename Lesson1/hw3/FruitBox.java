462e33cc74299c60a5d7e6f8bca343c790eef530
BlackTok <nu.pomytkin@gmail.com>
2021-02-12 16:09:51 +0500

    Lesson 1

---
+++ Lesson1/hw3/FruitBox.java
@@ -0,0 +1,58 @@
+package Lesson1.hw3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FruitBox<T extends Fruit> {
+    private final List<T> content = new ArrayList<>();
+
+    /**
+     * Метод добавляет фрукт определенного типа в коробку
+     * @param fruit экземпляр класса, расширяющего класс Fruit
+     */
+    public void addFruit(T fruit) {
+        content.add(fruit);
+    }
+
+    /**
+     * Возвращает вес коробки
+     * @return вес типа float
+     */
+    public float getWeight() {
+        float weight = 0;
+
+        for (T t : content) {
+            weight += t.getWeight();
+        }
+
+        return weight;
+    }
+
+    /**
+     * Сравнивает вес двух коробок с фруктами
+     * @param box экземпляр класса, расширяющего класс Fruit
+     * @return true, если вес коробок равен
+     */
+    public <B extends Fruit> boolean compare(FruitBox<B> box) {
+        return this.getWeight() == box.getWeight();
+    }
+
+    /**
+     * очищает коробку
+     */
+    private void clearBox() {
+        this.content.clear();
+    }
+
+    /**
+     * Пересыпает фрукты одинакового типа из данной коробки в коробку, переданную в качестве параметра
+     * @param newBox экземпляр класса, расширяющего класс Fruit
+     */
+    public void replaceFruit(FruitBox<T> newBox) {
+        for (T fruit : content) {
+            newBox.addFruit(fruit);
+        }
+
+        clearBox();
+    }
+}
---
+++ Lesson1/hw3/Orange.java
@@ -0,0 +1,7 @@
+package Lesson1.hw3;
+
+public class Orange extends Fruit {
+    public Orange(float weight) {
+        super(weight);
+    }
+}
