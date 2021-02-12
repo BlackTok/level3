68869849dfd99e716620b4970db42abdf5037fcf
BlackTok <nu.pomytkin@gmail.com>
2021-02-12 16:15:30 +0500

    Lesson 1, изменил ошибки в описании функций

--- Lesson1/hw3/FruitBox.java
+++ Lesson1/hw3/FruitBox.java
@@ -30,7 +30,7 @@ public class FruitBox<T extends Fruit> {
 
     /**
      * Сравнивает вес двух коробок с фруктами
-     * @param box экземпляр класса, расширяющего класс Fruit
+     * @param box экземпляр класса, расширяющего класс Box
      * @return true, если вес коробок равен
      */
     public <B extends Fruit> boolean compare(FruitBox<B> box) {
@@ -46,7 +46,7 @@ public class FruitBox<T extends Fruit> {
 
     /**
      * Пересыпает фрукты одинакового типа из данной коробки в коробку, переданную в качестве параметра
-     * @param newBox экземпляр класса, расширяющего класс Fruit
+     * @param newBox экземпляр класса Box
      */
     public void replaceFruit(FruitBox<T> newBox) {
         for (T fruit : content) {
